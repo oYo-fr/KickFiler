d41a50b980019b24eede98bbbb72dc10f212c718
yoannd <yoann.diguet@gmail.com>
2019-08-29 01:34:25 +0200

    Add handlebars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import * as Handlebars from 'handlebars/dist/handlebars';
+import swaggerSample from '../assets/swagger.json';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,7 @@ export class AppComponent {
   input: string = "    {{#each definitions}}  {{@key}}  {{/each}}";
   output: string = '';
   editorOptions = {theme: 'vs-dark', language: 'csharp'};
+  handlebarsEditorOptions = {theme: 'vs-dark', language: 'handlebars'}
   view = {
     button: 'This is the name of a button',
     numbers: [
@@ -18,6 +21,10 @@ export class AppComponent {
         {name: 'Three', value: 3},
     ]
   };
+  public constructor(){
+    this.output = Handlebars.compile(this.input)(swaggerSample);
+  }
   onKey(event: any) { // without type info
+    this.output = Handlebars.compile(this.input)(swaggerSample);
   }
 }
